10baab5660f24d436a69c13390775a142cc39606
stryn26 <sutrionoarif@gmail.com>
2022-07-03 11:18:53 +0700

    master

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+const {
+  addNoteHandler, getAllNotesHandler, getNotesByIdHandler, editNotesByIdHandler, deleteByIdHandler,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: addNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: getNotesByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: editNotesByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: deleteByIdHandler,
+  },
+];
+
+module.exports = routes;
